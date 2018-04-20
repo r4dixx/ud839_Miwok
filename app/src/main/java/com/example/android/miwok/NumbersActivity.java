1360fe0b0ba1b4412c77ced5a6fcc6fc6c9f178c
r4dixx <amael.sikel@gmail.com>
2018-04-20 12:30:44 +0200

    Switched from Array to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -13,6 +13,7 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+/*
         String[] numbersList = new String[10];
         numbersList[0] = getResources().getString(R.string.numbers_1);
         numbersList[1] = getResources().getString(R.string.numbers_2);
@@ -24,5 +25,14 @@ public class NumbersActivity extends AppCompatActivity {
         numbersList[7] = getResources().getString(R.string.numbers_8);
         numbersList[8] = getResources().getString(R.string.numbers_9);
         numbersList[9] = getResources().getString(R.string.numbers_10);
+
+        Log.w("Numbers Activity", "number at first position: " + numbersList[0]);
+        Log.w("Numbers Activity", "number at mid position: " + numbersList[4]);
+        Log.w("Numbers Activity", "number at last position: 0 " + numbersList[9]);
+*/
+
+        ArrayList<String> numbersList = new ArrayList<>();
+        numbersList.add(getResources().getString(R.string.numbers_1) + getResources().getString(R.string.numbers_2) + getResources().getString(R.string.numbers_3) + getResources().getString(R.string.numbers_4) + getResources().getString(R.string.numbers_5) + getResources().getString(R.string.numbers_6) + getResources().getString(R.string.numbers_7) + getResources().getString(R.string.numbers_8) + getResources().getString(R.string.numbers_9) + getResources().getString(R.string.numbers_10));
+
     }
 }
