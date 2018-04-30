a1d4b69648ad962df9686a1d5a4edf7a72913170
r4dixx <amael.sikel@gmail.com>
2018-04-30 12:15:51 +0200

    Added TextViews to layout with while loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -13,38 +17,27 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-/*
-        String[] numbersList = new String[10];
-        numbersList[0] = getResources().getString(R.string.numbers_1);
-        numbersList[1] = getResources().getString(R.string.numbers_2);
-        numbersList[2] = getResources().getString(R.string.numbers_3);
-        numbersList[3] = getResources().getString(R.string.numbers_4);
-        numbersList[4] = getResources().getString(R.string.numbers_5);
-        numbersList[5] = getResources().getString(R.string.numbers_6);
-        numbersList[6] = getResources().getString(R.string.numbers_7);
-        numbersList[7] = getResources().getString(R.string.numbers_8);
-        numbersList[8] = getResources().getString(R.string.numbers_9);
-        numbersList[9] = getResources().getString(R.string.numbers_10);
+        ArrayList<String> words = new ArrayList<>();
 
-        Log.w("Numbers Activity", "number at first position: " + numbersList[0]);
-        Log.w("Numbers Activity", "number at mid position: " + numbersList[4]);
-        Log.w("Numbers Activity", "number at last position: 0 " + numbersList[9]);
-*/
+        words.add(getResources().getString(R.string.numbers_1));
+        words.add(getResources().getString(R.string.numbers_2));
+        words.add(getResources().getString(R.string.numbers_3));
+        words.add(getResources().getString(R.string.numbers_4));
+        words.add(getResources().getString(R.string.numbers_5));
+        words.add(getResources().getString(R.string.numbers_6));
+        words.add(getResources().getString(R.string.numbers_7));
+        words.add(getResources().getString(R.string.numbers_8));
+        words.add(getResources().getString(R.string.numbers_9));
+        words.add(getResources().getString(R.string.numbers_10));
 
-        ArrayList<String> numbersList = new ArrayList<>();
-        numbersList.add(getResources().getString(R.string.numbers_1));
-        numbersList.add(getResources().getString(R.string.numbers_2));
-        numbersList.add(getResources().getString(R.string.numbers_3));
-        numbersList.add(getResources().getString(R.string.numbers_4));
-        numbersList.add(getResources().getString(R.string.numbers_5));
-        numbersList.add(getResources().getString(R.string.numbers_6));
-        numbersList.add(getResources().getString(R.string.numbers_7));
-        numbersList.add(getResources().getString(R.string.numbers_8));
-        numbersList.add(getResources().getString(R.string.numbers_9));
-        numbersList.add(getResources().getString(R.string.numbers_10));
+        LinearLayout rootView = findViewById(R.id.rootView);
 
-        Log.w("Numbers Activity", "Word at first index: " + numbersList.get(0));
-        Log.w("Numbers Activity", "Word at last index: " + numbersList.get(numbersList.size() - 1));
-
+        int index = 0;
+        while (index < words.size()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
     }
 }
