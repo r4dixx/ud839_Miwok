3e75dc6211931c4f40c6e157fd2222917d1e3d2b
r4dixx <amael.sikel@gmail.com>
2018-06-01 23:30:31 +0200

    Added illustrations for every word activities except the PhrasesActivity

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,10 +1,12 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -51,6 +53,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView defaultTextView = listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
+        ImageView imageView = listItemView.findViewById((R.id.image));
+
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getmImageResourceId());
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
         // Return the whole list item layout (containing 2 TextViews so it can be show in the ListView
         return listItemView;
     }
